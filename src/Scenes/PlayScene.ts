0e56a265aeda1bbcb99e525caf00bc68c6fb69e1
themtrx <themtrx@mail.bg>
2023-10-05 11:42:28 +0300

    Types fundamnetals. Making player jump and adding gravity.

--- src/Scenes/PlayScene.ts
+++ src/Scenes/PlayScene.ts
@@ -1,6 +1,8 @@
-import Phaser from "phaser"
+import { SpriteWithDynamicBody } from "../types"
 
 class PlayScene extends Phaser.Scene {
+
+    player: SpriteWithDynamicBody
 
     get gameHeight() {
         return this.game.config.height as number
@@ -13,6 +15,7 @@ class PlayScene extends Phaser.Scene {
     create() {
         this.createEnvironment()
         this.createPlayer()
+        this.registerPlayerControl()
     }
 
     createEnvironment() {
@@ -20,7 +23,17 @@ class PlayScene extends Phaser.Scene {
     }
 
     createPlayer() {
-        this.physics.add.sprite(0, this.gameHeight, 'dino-idle').setOrigin(0, 1)
+        this.player = this.physics.add.sprite(0, this.gameHeight, 'dino-idle').setOrigin(0, 1)
+
+        this.player.setGravityY(500).setCollideWorldBounds(true).setBodySize(44, 92)
+    }
+
+    registerPlayerControl() {
+        const spaceBar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE)
+
+        spaceBar.on('down', () => {
+            this.player.setVelocityY(-300)
+        })
     }
     
 }
